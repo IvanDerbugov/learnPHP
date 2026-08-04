581581469585844ba435da2e036dc393b23bcaab
IvanDerbugov <i.derbugoff2001@gmail.com>
2026-08-04 11:18:09 +0300

    проверка на пустую строку

--- POST/user.php
+++ POST/user.php
@@ -1,19 +1,22 @@
 <?
+require_once __DIR__ . '/../error-config.php';
 $userName = 'user name don\'t definet';
-$userName = 'user age don\'t definet';
+$userAge = 'user age don\'t definet';
 
-if (isset($_POST['userName'])) {
+if (isset($_POST['userName']) && $_POST['userName'] != '') {//в POST строки всегда
     $userName = $_POST['userName'];
     //method added user name in database
-    echo 'User ' . $userName . ' added in database' . "<br/>";
+    echo 'User ' . '\'' . $userName . '\'' . ' added in database' . "<br/>";
 
-    if (isset($_POST['userAge'])) {
+    if (isset($_POST['userAge']) && $_POST['userAge'] !== '') {
         //method added Age user in database
-        echo 'Age from user ' . $userName . 'added in database';
+        echo 'Age from user ' . '\'' . $userName . '\'' . ' added in database';
     } else {
         echo $userAge . "<br/>";
     } 
 } else {
     echo $userName . "<br/>";
     echo $userAge . "<br/>";
-}
+};
+echo "<br/>" . '======' . "<br/>";
+var_dump($_POST); //строки
