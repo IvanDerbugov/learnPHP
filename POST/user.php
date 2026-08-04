c7b619f61468735ca0dce3d019c86acba4761f1f
IvanDerbugov <i.derbugoff2001@gmail.com>
2026-08-04 22:32:15 +0300

    безопасность - чтобы при вводе в форму не выполнился скрипт, если такой загрузят

--- POST/user.php
+++ POST/user.php
@@ -4,12 +4,16 @@ $userName = 'user name don\'t definet';
 $userAge = 'user age don\'t definet';
 
 if (isset($_POST['userName']) && $_POST['userName'] != '') {//в POST строки всегда, не важно как сравнивать
-    $userName = trim($_POST['userName']);
+    $userName = strip_tags(
+        trim($_POST['userName'])
+    );
     //method added user name in database
     echo 'User ' . '\'' . $userName . '\'' . ' added in database' . "<br/>";
 
     if (isset($_POST['userAge']) && $_POST['userAge'] !== '') {
-        $userAge = trim($_POST['userAge']);
+        $userAge = strip_tags(
+            trim($_POST['userAge'])
+        );
         //method added Age user in database
         echo 'Age from user ' . '\'' . $userAge . '\'' . ' added in database';
     } else {
@@ -20,6 +24,6 @@ if (isset($_POST['userName']) && $_POST['userName'] != '') {//в POST строк
     echo $userAge . "<br/>";
 };
 echo "<br/>" . '======' . "<br/>";
-print_r($_POST); //не показывает тип
-echo "<br/>" . '-----------' . "<br/>";
+// print_r($_POST); //не показывает тип
+// echo "<br/>" . '-----------' . "<br/>";
 var_dump($_POST); //строки
