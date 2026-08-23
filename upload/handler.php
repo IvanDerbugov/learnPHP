c93feb56eae08628f050c3cd5a88dbc1d27e78b6
IvanDerbugov <i.derbugoff2001@gmail.com>
2026-08-23 22:46:02 +0300

    разбирался с выгрузкой файлов на сервер

--- upload/handler.php
+++ upload/handler.php
@@ -1,4 +1,7 @@
 <?
 require_once __DIR__ . '/../error-config.php';
-
+    echo '$_FILES: '  . "<br/>";
+    var_dump($_FILES); //сюда прилетают все данные с форм enctype =multipart/form-data
+    echo "<br/>" . '$_POST: '  . "<br/>";
+    var_dump($_POST); // но тут будет пустто, тк сервер направил бинарный файл в суперглобальный массив $_FILES сразу из-за html атрибута
 ?>
