72d49ee0a0e7e275c6aeaedf99063b7b1e974d88
IvanDerbugov <i.derbugoff2001@gmail.com>
2026-08-31 11:19:58 +0300

    добавил ограничение по размеру загружаемого файла и перевёл на свич кейсы ифы

--- upload/handler.php
+++ upload/handler.php
@@ -5,22 +5,38 @@ var_dump($_FILES); //сюда прилетают все данные с форм
 echo "<br/>" . '$_POST: ' . "<br/>";
 var_dump($_POST); // но тут будет пустто, тк сервер направил бинарный файл в суперглобальный массив $_FILES сразу из-за html атрибута
 
-if ($_FILES && $_FILES['filename']['error'] == UPLOAD_ERR_OK) { // или можно было $_FILES['filename']['error'] == 0 (UPLOAD_ERR_OK == 0)
-    $name = $_FILES['filename']['name'];
-    echo "<br/>" . "<br/>" . '=============' . "<br/>";
+if ($_FILES) {
+    switch ($_FILES['filename']['error']) { // код ошибки: 0 = UPLOAD_ERR_OK, дальше константы ниже
+        case UPLOAD_ERR_OK:
+            $name = $_FILES['filename']['name'];
+            echo "<br/>" . "<br/>" . '=============' . "<br/>";
 
-    $path = __DIR__ . '/files/'; //путь для сохранения
-    function saveFile()
-    {
-        global $path, $name;
-        move_uploaded_file($_FILES['filename']['tmp_name'], $path . $name);
+            $path = __DIR__ . '/files/'; //путь для сохранения
+            function saveFile()
+            {
+                global $path, $name;
+                move_uploaded_file($_FILES['filename']['tmp_name'], $path . $name);
+            }
+            if (is_dir($path)) {
+                saveFile();
+            } else {
+                mkdir($path, 0755, true);
+                saveFile();
+            }
+            echo "<br/>" . 'Файл успешно загружен';
+            phpinfo();
+            break;
+        case UPLOAD_ERR_INI_SIZE:
+        case UPLOAD_ERR_FORM_SIZE:
+            echo "<br/>" . 'Файл слишком большой';
+            break;
+        case UPLOAD_ERR_PARTIAL:
+            echo "<br/>" . 'Файл загружен частично';
+            break;
+        case UPLOAD_ERR_NO_FILE:
+        case UPLOAD_ERR_NO_TMP_DIR:
+            echo "<br/>" . 'Файл не загружен';
+            break;
     }
-    if (is_dir($path)) {
-        saveFile();
-    } else {
-        mkdir($path, 0755, true);
-        saveFile();
-    }
-    echo "<br/>" . 'Файл успешно загружен';
 }
 ?>
