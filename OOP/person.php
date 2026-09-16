a1807c8584cf33c12143891dc84477bcd67fa9a5
IvanDerbugov <i.derbugoff2001@gmail.com>
2026-09-17 00:03:44 +0300

    добавил подстраховку на случай если не будет error-config.php. через include_once не подойдёт - тк не увидим на экране что провал случился. отработал запрет переопределения final, можно давать его так же классу всему и тогда расширение класса данным классом будет невозможно. проверил пренадлежность инстансов через instanceof

--- OOP/person.php
+++ OOP/person.php
@@ -1,5 +1,9 @@
 <?
-require_once $_SERVER['DOCUMENT_ROOT'] . '/learn-php/error-config.php';
+
+$path = $_SERVER['DOCUMENT_ROOT'] . '/learn-php/error-config.php';
+if (file_exists($path)) {
+    require_once $path;
+} else echo 'error-config.php НЕ НАЙДЕН!' . "<br/>";
 
 class Person
 {
@@ -9,7 +13,8 @@ class Person
         $this->age = $age;
     }
 
-    function sayHi () {
+    function sayHi()
+    {
         echo "Привет, {$this->name}!<br><br>";
     }
 
@@ -24,21 +29,27 @@ class Person
         }
         echo "<br>";
     }
+
+    final function lastFunction() {
+        echo 'конец класса'  . "<br/>"  . "<br/>";
+    }
 }
 
 $Ivan = new Person('Ivan', 25);
-$Ivan -> sayHi();
-$Ivan -> analysisLetterName();
+$Ivan->sayHi();
+$Ivan->analysisLetterName();
 
 class Usergame extends Person
 {
-    function __construct($name, $age, public $gameLvl = 1, public $score = 0) {
+    function __construct($name, $age, public $gameLvl = 1, public $score = 0)
+    {
         parent::__construct($name, $age);
         $this->gameLvl = $gameLvl;
         $this->score = $score;
     }
 
-    function sayHi() {
+    function sayHi()
+    {
         echo "Приветствую тебя в скайриме, {$this->name}!<br><br>";
     }
 
@@ -47,8 +58,33 @@ class Usergame extends Person
         echo "Каждая буква твоего имени содержит уникальнкую силу...<br>";
         parent::analysisLetterName();
     }
+
+    //положит стр с 500-ой
+    // function lastFunction() {
+    //     parent::lastFunction();
+    // }
+    
 }
 
 $Ivan2000 = new Usergame('Ivan2000', 25, score: 100);
-$Ivan2000 -> sayHi();
-$Ivan2000 -> analysisLetterName();
+$Ivan2000->sayHi();
+$Ivan2000->analysisLetterName();
+$Ivan2000->lastFunction();
+
+if ($Ivan instanceof Person) {
+    echo '$Ivan пренадлежит Person' . "<br/>";
+} else
+    echo '$Ivan НЕ пренадлежит Person' . "<br/>";
+if ($Ivan instanceof Usergame) {
+    echo '$Ivan пренадлежит Usergame' . "<br/>";
+} else
+    echo '$Ivan НЕ пренадлежит Usergame' . "<br/>";
+
+if ($Ivan2000 instanceof Person) {
+    echo '$Ivan2000 пренадлежит Person' . "<br/>";
+} else
+    echo '$Ivan2000 НЕ пренадлежит Person' . "<br/>";
+if ($Ivan2000 instanceof Usergame) {
+    echo '$Ivan2000 пренадлежит Usergame' . "<br/>";
+} else
+    echo '$Ivan2000 НЕ пренадлежит Usergame' . "<br/>";
